17c908b86cfffcd6b4018042cfb3fd3af13dc14f
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-11-20 17:33:26 +0100

    Constans add to send a message Locationuser Saved in SaveLocationUserCommand

---
+++ B4.PE3.RodriguezA/B4.PE3.RodriguezA/Constants/MessageNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.PE3.RodriguezA.Constants
+{
+   public static class MessageNames
+    {
+                 /// <summary>
+            /// MessagingCenter key to notify a bucket has been saved
+           /// </summary>
+            public const string LocationUserSaved = "LOCATIONUSERSAVED";
+        
+    }
+}
